deaea35d6cc07080afb9c89473dab71fb3143d78
soumya1702 <114746817+soumya1702@users.noreply.github.com>
2024-09-25 15:05:06 -0400

    model KNN for accuracy, classification, confusion matrix

--- model_KNN.py
+++ model_KNN.py
@@ -17,6 +17,14 @@ y_pred_knn = knn_model.predict(X_test)
 print("KNN Accuracy: ", accuracy_score(y_test, y_pred_knn))
 print(classification_report(y_test, y_pred_knn))
 
+# Overall metrics for the tuned Decision Tree model
+print("Tuned Decision Tree Performance:")
+print("Overall Accuracy:", metrics.accuracy_score(y_test, y_pred_knn))
+print("Overall Precision:", metrics.precision_score(y_test, y_pred_knn, average='weighted'))
+print("Overall Recall:", metrics.recall_score(y_test, y_pred_knn, average='weighted'))
+print("Overall F1 Score:", metrics.f1_score(y_test, y_pred_knn, average='weighted'))
+
+
 # Confusion Matrix for KNN
 cm_knn = confusion_matrix(y_test, y_pred_knn)
 sns.heatmap(cm_knn, annot=True, fmt='d', cmap='Blues')
